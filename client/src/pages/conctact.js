4694ce0410638efebbf5d8fe3f68007a5f9962c8
ertugrul.gocer <ertugrul09gocer@gmail.com>
2022-04-15 11:21:44 +0300

    Conctact Page Complate

--- client/src/pages/conctact.js
+++ client/src/pages/conctact.js
@@ -1,16 +1,12 @@
 import React from "react";
+import "../styles/ConctactPage/conctact.css";
 import {
     Container,
     Typography,
     Grid,
+    Box,
+    Divider
 } from "@mui/material";
-import "../styles/ConctactPage/conctact.css";
-import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
-import MonochromePhotosOutlinedIcon from '@mui/icons-material/MonochromePhotosOutlined';
-import ConnectWithoutContactOutlinedIcon from '@mui/icons-material/ConnectWithoutContactOutlined';
-import NotListedLocationOutlinedIcon from '@mui/icons-material/NotListedLocationOutlined';
-import ContactPhoneOutlinedIcon from '@mui/icons-material/ContactPhoneOutlined';
-import ContactMailOutlinedIcon from '@mui/icons-material/ContactMailOutlined';
 
 const Conctact = () => {
     return (
@@ -21,63 +17,37 @@ const Conctact = () => {
                 </Typography>
             </Container>
             <Container maxWidth="xl">
-                <h3>Bize ulaşmak neden önemli?</h3>
-                <Typography>
+                <h2>
+                    Bize ulaşmak neden önemli?
+                </h2>
+                <Typography className="text">
                     Sizde artık birşeyleri değiştirmek istemiyor musunuz? Çoğu insan Fitness salonlarını tercih ediyor. Ama fitness salonuna gitmeden forma girmek de mümkün. Size özel diyet programı ve evde ister aletli ister aletsiz hocalarımız tarafından hazırlanan bir program karşılıyor. Sürekli hocalarımız ile irtibatda kalarak anlık takib de mümkün. Kısacası en iyi PT(Personal Training) ile artık her şey daha kolay.
                 </Typography>
-                <h3>
+                <h2>
                     Bize nasıl ulaşabilirsiniz ?
-                </h3>
-                <Typography>
-                    Sayfada yer alan whatsapp ikonuna tıklayarak canlı iletişime geçebilirsiniz. İnstagram adresimizden 'enesuludoganfitness' canlı takipde kalabilir, bizleri takip edebilirsiniz.                </Typography>
+                </h2>
+                <Typography className="text">
+                    Sayfada yer alan whatsapp ikonuna tıklayarak canlı iletişime geçebilirsiniz. Aşağıdaki adreslerimizden bizlere ulaşabilirsiniz. İnstagram adresimizden 'enesuludoganfitness' canlı takipde kalabilir, bizleri takip edebilirsiniz.
+                </Typography>
             </Container>
             {/* Bilgiler */}
-            <Grid container>
-                <Grid item md={6} textAlign='center'>
-                    <Typography>Sosyal Medya Adreslerimiz</Typography>
-                    <Grid container direction={'row'}>
-
-                        <Grid container md={6}>
-                            <Grid md={6}>
-                                İnstagram
-                            </Grid>
-                        </Grid>
-                        <Grid md={6}>
-                            <Grid md={6}>
-                                enesuludoganfitness
-                            </Grid>
-                        </Grid>
+            <Box padding={'20px'} height="262.5px">
+                <Grid item xs={12} md={12} className="conctactHead">
+                    İletişim Bilgilerimiz
+                </Grid>
+                <Grid container direction={'row'} textAlign={"center"} paddingTop="10px" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid xs={2.3} md={6} className="conctactText">
+                        <Grid item paddingTop={'10px'}>Telefon</Grid>
+                        <Grid item paddingTop={'10px'}>İnstagram</Grid>
+                        <Grid xs={1} sm={11} md={12} item paddingTop={'10px'}>Mail</Grid>
+                    </Grid>
+                    <Grid container direction={'column'} xs={9.7} md={6} className="conctactText">
+                        <Grid item paddingTop={'10px'}>+90 553 917 0698</Grid>
+                        <Grid item paddingTop={'10px'}>enesuludoganfitness</Grid>
+                        <Grid item xs={1} paddingTop={'10px'}>enesuludoganfitness@gmail.com</Grid>
                     </Grid>
                 </Grid>
-                <Grid item md={6} textAlign='center'>
-                    <Typography>Sosyal Medya Adreslerimiz</Typography>
-                    <Grid container direction={'row'}>
-
-                        <Grid md={6}>
-                            <Grid>
-                                deneme1
-                            </Grid>
-                            <Grid>
-                                deneme2
-                            </Grid>
-                            <Grid>
-                                deneme3
-                            </Grid>
-                        </Grid>
-                        <Grid md={6}>
-                            <Grid>
-                                denek1
-                            </Grid>
-                            <Grid>
-                                denek2
-                            </Grid>
-                            <Grid>
-                                denek3
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
+            </Box>
         </>
     );
 }
