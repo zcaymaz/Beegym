34280b2d4a534720ebe261d4ac9e67e02869283b
euludogn <enveruludogan@gmail.com>
2022-05-14 01:17:14 +0300

    blog page modal eklendi responsivlik düzeltildi.

--- client/src/components/common/Blogcard.js
+++ client/src/components/common/Blogcard.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
-import { Typography, Grid, Box, Card, CardMedia, IconButton, Divider, CardActions, Modal, CardContent, Button } from '@mui/material'
+import React from 'react'
+import { Typography, Grid, Box, Fade, Card, CardMedia,IconButton, Divider, CardActions, Modal, CardContent, Button } from '@mui/material'
 import CloseIcon from '@mui/icons-material/Close'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 const style = {
-    maxWidth: '640px',
+    maxWidth: '660px',
     minHeight: '523px',
     bgcolor: 'background.paper',
     borderRadius: '20px',
@@ -15,29 +15,15 @@ const style = {
 const styles = {
     paddingTop: '8%',
     overflowX: 'hidden',
+    paddingBottom: '8%'
 };
 
-
-
 const Blogcard = (props) => {
-    const [showModal, setShowModal] = useState(false);
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
-    const [selectedValue, setSelectedValue] = React.useState('a');
-    const handleChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
-    const controlProps = (item) => ({
-        checked: selectedValue === item,
-        onChange: handleChange,
-        value: item,
-        name: 'color-radio-button-demo',
-        inputProps: { 'aria-label': item },
-    });
 
     return (
-
         <Grid xs={12} sm={6} md={4} lg={3}
             className='blogCardData'
         >
@@ -45,34 +31,28 @@ const Blogcard = (props) => {
                 <CardMedia
                     className='Cardimage'
                     component="img"
-                    height="240"
+                    height="260"
                     image={props.media}
-                // {require('../images/cardimage1.png')}
                 />
                 <Box className='blogCardHeader' >
                     <Typography className='Cardtext'>
                         {props.header}
-                        {/* YOU MAY BE <br /> FORGETTING TO DRINK <br /> WATER! */}
                     </Typography>
                 </Box>
                 <CardContent sx={{ height: '180px' }}>
                     <Typography className='Cardtextcontent' >
                         {props.content}
-                        {/* Su yaşamın vazgeçilmez bir parçası fakat bunun ciddiyetinin farkında mıyız? <br /> <br />  Nefes almaktan sonra en çok ihtiyaç duyduğumuz şey su içmektir... */}
                     </Typography>
                 </CardContent>
                 <CardActions>
                     <Grid container
                         direction="row"
-
                     >
-
                         <Grid xs={12} md={4}
                             sx={{ textAlign: { xs: 'center', xl: 'normal' } }}
                         >
                             <Typography sx={{ paddingTop: '7px', }}>
                                 {props.date}
-                                {/* 14.02.2022 */}
                             </Typography>
                         </Grid>
                         <Grid xs={12} md={8}
@@ -89,20 +69,22 @@ const Blogcard = (props) => {
                             open={open}
                             onClose={handleClose}
                             sx={styles}
+                            className='blogmodal'                      
                         >
                             <center>
+                            <Fade in={open}>
                                 <Box sx={{ ...style, }}>
                                     <IconButton onClick={() => handleClose()} sx={{ float: 'right' }}>
-                                        <CloseIcon sx={{ color: '#FAA629' }} fontSize="medium" />
+                                        <CloseIcon sx={{ color: '#FFD358' }} fontSize="medium" />
                                     </IconButton>
-                                    <Typography variant='h5' className='viewSubmissionchidmodaltitle'>
+                                    <Typography variant='h5'>
                                         {props.modalheader}
                                     </Typography>
                                     <Divider sx={{ border: '1px solid #C0C0C0', marginTop: '10px' }} />
-                                    <Typography variant='h6'>
+                                    <Typography variant='h6' >
                                         {props.modalcontentheader}
                                     </Typography>
-                                    <Typography>
+                                    <Typography >
                                         {props.modalcontenttext}
                                     </Typography>
                                     <Divider sx={{ border: '1px solid #C0C0C0', }} />
@@ -112,14 +94,42 @@ const Blogcard = (props) => {
                                     <Typography>
                                         {props.modalcontenttext2}
                                     </Typography>
+                                    <Divider sx={{ border: '1px solid #C0C0C0', }} />
+                                    <Typography variant='h6'>
+                                        {props.modalcontentheader3}
+                                    </Typography>
+                                    <Typography>
+                                        {props.modalcontenttext3}
+                                        <Divider sx={{ border: '1px solid #C0C0C0', }} />
+                                    </Typography>
+                                    <Typography variant='h6'>
+                                        {props.modalcontentheader4}
+                                    </Typography>
+                                    <Typography>
+                                        {props.modalcontenttext4}
+                                        <Divider sx={{ border: '1px solid #C0C0C0', }} />
+                                    </Typography>
+                                    <Typography variant='h6'>
+                                        {props.modalcontentheader5}
+                                    </Typography>
+                                    <Typography>
+                                        {props.modalcontenttext5}
+                                        <Divider sx={{ border: '1px solid #C0C0C0', }} />
+                                    </Typography>
+                                    <Typography variant='h6'>
+                                        {props.modalcontentheader6}
+                                    </Typography>
+                                    <Typography>
+                                        {props.modalcontenttext6}
+                                    </Typography>
                                 </Box>
+                                </Fade>
                             </center>
                         </Modal>
                     </Grid>
                 </CardActions>
             </Card>
         </Grid>
-
     )
 }
 
--- client/src/pages/blog.js
+++ client/src/pages/blog.js
@@ -1,106 +1,48 @@
 import React from 'react'
-import { Typography,Modal, Box, Divider, Grid } from '@mui/material'
+import { Box, CardMedia, Grid } from '@mui/material'
 import BlogCard from '../components/common/Blogcard'
-import {cardimage1,
-        cardimage2,
-        cardimage3,
-        cardimage4,
-        cardimage5,
-         } from '../images'
-
-
-
-const blogCardData = [
-    {
-        header: " Kilo ve Yağ Kaybı",
-        content: "Yağ kaybı, yağdan kilo kaybı anlamına gelir. Kilo vermekten daha spesifik ve sağlıklı bir hedeftir.",
-        media: cardimage1,
-        date: " 14.02.2022 ",
-        button: "DEVAMINI OKU",
-        modalheader: "Kilo Vermek ve Yağ Yakmak Arasındaki Fark Nedir? Kilo ve Yağ Kaybı",
-        modalcontentheader: "Kilo Kaybı Nedir?",
-        modalcontenttext: "Kilo kaybı, genel vücut ağırlığınızdaki kas, su ve yağ kayıplarından kaynaklanan azalmayı ifade eder. Bununla birlikte, yağdan mı yoksa kastan mı kilo verdiğinizi bilmek zor olabilir.",
-        modalcontentheader2: "Yağdan Kilo Verdiğimizi Nasıl Anlarız?",
-        modalcontenttext2 : "Bir tartı kullanarak kilo verme durumunuzu takip etmek yaygın bir uygulamadır. Tartı kullanımı kilonuzu öğrenmek için yardımcı olabilirken çoğu tartı, yağ kaybı ile kas kaybı arasında ayrım yapmaz. Bu nedenle, yalnızca tartı ile kilonuzu takip etmek, yağ mı yoksa kas mı kaybettiğinizi ve hangi miktarlarda kaybettiğinizi belirlemenin güvenilir bir yolu değildir. Vücut yağı da ölçen tartı, sahip olduğunuz yağ ve kas yüzdesini ölçerek vücut kompozisyonunuzun daha doğru bir resmini sağlayabilir. Vücut yağ yüzdenizi tahmin etmek için deri kıvrım kaliperleri de kullanabilirsiniz, ancak bu, doğruluğu sağlamak için pratik gerektirir."
-    },
-    {
-        header: " GÜZEL BİR UYKU UYUYAMIYOR MUSUNUZ ? ",
-        content: "Sizinle “MELATONİN” ile ilgili bir kaç paylaşımda bulunmak istiyorum. Uyku problemi yaşam kalitemizi...",
-        media: cardimage2,
-        date: " 14.02.2022 ",
-        button: "DEVAMINI OKU",
-        modalheader: "GÜZEL BİR UYKU UYUYAMIYOR MUSUNUZ?",
-
-    },
-    {
-        header: " Şeker Tüketimi ",
-        content: "Uzun zamandır şekerin yararları ve zararları tartışılırken; bu bloğumuzda yer verdik",
-        media: cardimage3,
-        date: " 14.02.2022 ",
-        button: "DEVAMINI OKU",
-        modalheader: "ŞEKER TÜKETIMI",
-
-    },
-    {
-        header: "Sebze Programı ",
-        content: "Sağlıklı beslenme başlı başına ciddi bir iştir. Yağ yakmak ve kas yapmak söz konusu olduğunda daha da karmaşık hale gelir.",
-        media: cardimage4,
-        date: " 14.02.2022 ",
-        button: "DEVAMINI OKU",
-        modalheader: "SEBZE PROGRAMI",
-
-    },
-    {
-        header: "ESNEME TERİMİ NEDİR? ",
-        content: "Biz bu yazıda, kasları germe tanımıyla bir takım kaynaklardan bir araya getirdiğimiz streching terimini ifade edeceğiz...",
-        media: cardimage5,
-        date: " 14.02.2022 ",
-        button: "DEVAMINI OKU",
-        modalheader: "ESNEME TERİMİ NEDİR?",
-
-    },
- 
-]
-
+import BlogData from '../components/common/BlogData'
 
 const blog = () => {
     return (
-        <Box className='blogCardData'>
-            <Divider
-                sx={{
-                    backgroundColor: 'yellow'
-                }}
+        <Box sx={{bgcolor:'#4B4B4B'}}>
+            <CardMedia
+                component="img"
+                height="260"
+                sx={{marginBottom: '20px'}}
+                image={require('../images/blogPage.png')}
             />
-            <Box className='blogheader'>
-                <img
-                    className='blogimage'
-                    src={require('../images/blogPage.png')}
-                />
-            </Box>
             {/* Card */}
             <center>
-            <Grid container
-                direction='row'
-                className='blogCardData'
-                sx={{ justifyContent: { xs: 'center', md: 'normal' } }}
-            >
-                {blogCardData.map((card) => {
-                    return (
-                        <BlogCard
-                            header={card.header}
-                            content={card.content}
-                            media={card.media}
-                            date={card.date}
-                            button={card.button}
-                            modalheader={card.modalheader}
-                            modalcontentheader={card.modalcontentheader}
-                            modalcontenttext={card. modalcontenttext}
-                            modalcontentheader2={card.modalcontentheader2}
-                            modalcontenttext2={card.modalcontenttext2}
-                        />
-                    )
-                })}
-            </Grid>
+                <Grid container
+                    direction='row'                 
+                    sx={{ justifyContent: { xs: 'center', md: 'normal' },bgcolor:'#4B4B4B' }}
+                >
+                    {BlogData.map((card) => {
+                        return (
+                            <BlogCard
+                                header={card.header}
+                                content={card.content}
+                                media={card.media}
+                                date={card.date}
+                                button={card.button}
+                                modalheader={card.modalheader}
+                                modalcontentheader={card.modalcontentheader}
+                                modalcontenttext={card.modalcontenttext}
+                                modalcontentheader2={card.modalcontentheader2}
+                                modalcontenttext2={card.modalcontenttext2}
+                                modalcontentheader3={card.modalcontentheader3}
+                                modalcontenttext3={card.modalcontenttext3}
+                                modalcontentheader4={card.modalcontentheader4}
+                                modalcontenttext4={card.modalcontenttext4}
+                                modalcontentheader5={card.modalcontentheader5}
+                                modalcontenttext5={card.modalcontenttext5}
+                                modalcontentheader6={card.modalcontentheader6}
+                                modalcontenttext6={card.modalcontenttext6}
+                            />
+                        )
+                    })}
+                </Grid>
             </center>
         </Box>
     )
