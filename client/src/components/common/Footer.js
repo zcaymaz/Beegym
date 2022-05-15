4364e5001e5dc7778327b5848beb62a126fab02a
zcaymaz <zbyrcmyz@gmail.com>
2022-05-15 23:22:44 +0300

    loading

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,9 +1,35 @@
-import FooterBox from "./FooterBox"
+import { Grid, IconButton, Stack, Typography } from '@mui/material'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import EmailIcon from '@mui/icons-material/Email';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 export const Footer = () => {
     return (
         <>
-            <FooterBox />
+            <Grid container spacing={1} direction="row" justifyContent="center" alignItems="center" className='Footer'>
+                <Grid item xs={12}>
+                    <Typography sx={{ fontSize: '20px' }}>
+                        <i>ENES ULUDOGAN FİTNESS</i>
+                    </Typography>
+                </Grid>
+                <Stack direction={'row'}>
+                    <IconButton target={'_blank'} href={'https://www.instagram.com/enesuludoganfitness'}>
+                        <InstagramIcon fontSize='large' sx={{ color: 'white' }} />
+                    </IconButton>
+                    <IconButton target={'_blank'} href={'https://www.instagram.com/enesuludoganfitness'}>
+                        <WhatsAppIcon fontSize='large' sx={{ color: 'white' }} />
+                    </IconButton>
+                    <IconButton className='aciklama' href="#" data-title="enesuludoganfitness@gmail.com"  sx={{ color: 'white' }}>
+                        <EmailIcon fontSize='large' sx={{ color: 'white' }} />
+                    </IconButton>
+                </Stack>
+                <Grid item xs={12}>
+                    <Typography>
+                        {'Copyright © '}
+                        {new Date().getFullYear()} All Right Are Reserved{'.'}
+                    </Typography>
+                </Grid>
+            </Grid>
         </>
     )
 }
