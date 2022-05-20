b967260eb7ac694359a1d6d541fc18fb571cdc4b
ertugocer <ertugrul09gocer@gmail.com>
2022-05-20 23:28:38 +0300

    .

--- client/src/components/common/MutationCard.js
+++ client/src/components/common/MutationCard.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Card, CardMedia } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
+import React from 'react';
+import '../../styles/ChangesPage/Changes.css';
 
-const MutationCard = (props) => {
+const Changes = () => {
     return (
-        <>
-            <Card sx={{ height: '25rem',maxWidth:'400px',borderRadius:'20px'}}>
-                <CardMedia>
-                    <img
-                        className='mutationCard'
-                        alt='...'
-                        src={props.image}
-                    />
-                </CardMedia>
-            </Card>
-        </>
+        <Container className='bodyContainer'>
+        <Grid container direction={'row'}>
+        <Grid  lg={2} className='image'>
+            <Typography>asdajsdsdassadbjasbfas</Typography>
+        </Grid>
+                <Grid lg={9} className='life'>
+                <Typography>Merhabalar ben diyetisyen Şule Çetin. Hacettepe Üniversitesi Beslenme ve Diyetetik bölümünü 2018 yılında şeref öğrencisi olarak tamamladım.
+                    <br />
+                    Hemen sonrasında özel bir klinikte beslenme danışanmanlığı ve bölgesel zayıflama alanında hizmet vermeye başladım.
+                    Bu süreç boyunca yurtiçi ve yurtdışı yüz yüze görüşme imkanı sağlayamadığımız danışanlarıma online beslenme danışmanlığı vermeye devam ettim.</Typography>
+                </Grid>
+        </Grid>
+        </Container>
     )
 }
 
-export default MutationCard
+export default Changes
--- client/src/pages/mutation.js
+++ client/src/pages/mutation.js
@@ -1,33 +1,10 @@
 import React from 'react'
-import { Grid, Typography } from '@mui/material'
-import MutationCard from '../components/common/MutationCard'
-import {
-    antrenman,
-    beslenme,
-} from '../images'
+import Changes from '../components/common/MutationCard'
 
 const mutation = () => {
-    return (
-        <>
-            <Grid container spacing={1} direction='row' alignItems="center" sx={{ height: '30rem', bgcolor: 'red' }}>
-                {/* <Grid item xs={12} sx={{ bgcolor: 'red' }}>
-                    <Typography sx={{fontSize:'50px', textAlign:'center',fontFamily:'Mulish'}} className='Degisimler'>
-                        <i>DEĞİŞİMLER</i>
-                    </Typography>
-                </Grid> */}
-                <Grid item xs={4}>
-                    <MutationCard
-                        image={beslenme}
-                    />
-                </Grid>
-                <Grid item xs={4}>
-                    <MutationCard
-                        image={antrenman}
-                    />
-                </Grid>
-            </Grid>
-        </>
-    )
+  return (
+      <Changes />
+  )
 }
 
 export default mutation
